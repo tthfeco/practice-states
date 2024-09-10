dc980ff4ed85eb1138947668cbd7ac8cf7ba584f
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-10 12:06:00 +0200

    create pseudo code for registration form

--- state-practice/src/App.jsx
+++ state-practice/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <div>
+        <h3>Registration</h3>
+        <div>
+          <span>Name</span>
+          <button>Edit</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
